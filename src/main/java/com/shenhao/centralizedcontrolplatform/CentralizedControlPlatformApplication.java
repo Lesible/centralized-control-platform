a8aa9c7228aba2daafa63bd7ef514920bf9c5b5e
Lesible <1419318498@qq.com>
2023-04-23 17:15:17 +0800

    init

---
+++ src/main/java/com/shenhao/centralizedcontrolplatform/CentralizedControlPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.shenhao.centralizedcontrolplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedControlPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralizedControlPlatformApplication.class, args);
+    }
+
+}
