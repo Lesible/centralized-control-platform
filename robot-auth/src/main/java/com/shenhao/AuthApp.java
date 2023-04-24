027436e4b232ab80dd536f7058408e2729d0807e
Lesible <1419318498@qq.com>
2023-04-24 18:16:53 +0800

    two modules

---
+++ robot-auth/src/main/java/com/shenhao/AuthApp.java
@@ -0,0 +1,18 @@
+package com.shenhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 何嘉豪
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApp.class, args);
+    }
+
+}
